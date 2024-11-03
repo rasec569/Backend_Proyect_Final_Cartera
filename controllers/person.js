113550613f372e71dd064d6c3016a34cd5c9b7ad
Cesar Augusto Valencia Valezques <cesaraugusto_569@hotmail.com>
2024-11-03 01:06:15 -0500

    Validations and tests of the getPeople, getPersonById, updatePerson and deletePerson methods in the person controller and implementation of their routes

--- controllers/person.js
+++ controllers/person.js
@@ -56,42 +56,173 @@ export const createPerson = async (req, res) => {
 // Obtener todas las personas
 export const getPeople = async (req, res) => {
   try {
-    const people = await Person.find();
-    res.json(people);
+    // Obtener la página desde los parámetros de la solicitud
+    let page=req.params.page? parseInt(req.params.page, 10) :1;
+
+    // Obtener el número de personas por página desde los parámetros de la solicitud
+    let itemsPorPage=req.params.limit? parseInt(req.params.limit, 10) :10;
+    
+    const options={
+      page: page,
+      limit: itemsPorPage,
+      select: '-email -__v',
+      sort: {
+        nombres: 1,
+        apellidos: 1
+      }
+    }
+
+    // Obtener las personas paginadas
+    const people = await Person.paginate({}, options);
+
+    // Verificar si hay personas para mostrar
+    if (!people||people.docs.length===0){
+      return res.status(404).send({
+        status: "error",
+        message: "No hay personas para mostrar"
+      });
+    }
+
+    // Retorna los personas
+    return res.status(200).json({
+      status: "success",
+      message: "Persona encontrada",
+      people: people.docs,
+      totalDocs: people.totalDocs,
+      totalPages: people.totalPages,
+      currentPage: people.page,
+      itemsPerPage: people.limit
+    });
+    
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log("Error al obtener las persona", error);
+    res.status(500).json({
+      status: "error",
+      message: "Error al obtener las persona",
+      error: error.message
+    });
   }
 };
 
 // Obtener persona por ID
 export const getPersonById = async (req, res) => {
   try {
-    const person = await Person.findById(req.params.id);
-    if (!person) return res.status(404).json({ message: "Persona no encontrada" });
-    res.json(person);
+    // Obtener el id de la persona desde los parámetros de la solicitud
+    const personId = req.params.id;
+
+    // Buscar la persona en la base de datos y excluir ciertos campos
+    const person = await Person.findById(personId).select('-email -dirección -__v');
+
+    // Verificar si la persona existe
+    if (!person) {
+      return res.status(404).send({
+        status: "error",
+        message: "Persona no encontrada"
+      });
+    }
+
+    // Devolver la información de la persona
+    return res.status(200).json({
+      status: "success",
+      message: "Persona encontrada",
+      user: person
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log("Error al obtener la persona", error);
+    res.status(500).json({
+      status: "error",
+      message: "Error al obtener la persona",
+      error: error.message
+    });
   }
 };
 
 // Actualizar persona
 export const updatePerson = async (req, res) => {
   try {
-    const person = await Person.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!person) return res.status(404).json({ message: "Persona no encontrada" });
-    res.json(person);
+    const personId = req.params.id;
+    const personToUpdate = req.body;
+
+    // Validar campos requeridos
+    const requiredFields = ['tip_doc', 'doc_identifiacion', 'nombres', 'apellidos', 'email', 'telefono'];
+    for (const field of requiredFields) {
+      if (!personToUpdate[field]) {
+        return res.status(400).send({
+          status: "error",
+          message: `El campo ${field} es requerido.`,
+        });
+      }
+    }
+
+    // Verificar si se intenta actualizar el doc_identifiacion
+    const existingPerson = await Person.findOne({
+      doc_identifiacion: personToUpdate.doc_identifiacion,
+      _id: { $ne: personId } // Excluir el ID actual
+    });
+
+    // Si se intenta actualizar el doc_identifiacion y ya existe una persona con el mismo documento, retornar error
+    if (existingPerson) {
+      return res.status(409).send({
+        status: "error",
+        message: "El documento de identificación ya está en uso por otra persona.",
+      });
+    }
+
+    // Buscar y actualizar la persona
+    const updatedPerson = await Person.findByIdAndUpdate(personId, personToUpdate, { new: true });
+
+    // Verificar si la persona existe
+    if (!updatedPerson) {
+      return res.status(404).send({ 
+        status: "error",
+        message: "Persona no encontrada",
+      });
+    }
+
+    // Retornar la persona actualizada
+    return res.status(200).json({
+      status: "success",
+      message: "Persona actualizada con éxito",
+      person: updatedPerson,
+    });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    console.log("Error al actualizar la persona:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Error al actualizar la persona",
+      error: error.message,
+    });
   }
 };
 
 // Eliminar persona
 export const deletePerson = async (req, res) => {
   try {
-    const person = await Person.findByIdAndDelete(req.params.id);
-    if (!person) return res.status(404).json({ message: "Persona no encontrada" });
-    res.json({ message: "Persona eliminada" });
+    const personId = req.params.id;
+
+    // Buscar la persona para verificar si existe antes de eliminarla
+    const person = await Person.findById(personId);
+    if (!person) {
+      return res.status(404).send({
+        status: "error",
+        message: "Persona no encontrada",
+      });
+    }
+
+    // Eliminar la persona
+    await Person.findByIdAndDelete(personId);
+
+    // Retornar mensaje de éxito
+    return res.status(200).send({
+      status: "success",
+      message: "Persona eliminada con éxito",
+    });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log("Error al eliminar la persona:", error);
+    res.status(500).send({
+      status: "error",
+      message: "Error al eliminar la persona",
+      error: error.message,
+    });
   }
 };
